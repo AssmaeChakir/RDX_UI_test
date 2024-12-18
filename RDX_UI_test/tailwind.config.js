7e72a4f9b432d4cafbae80026d935cced2834aa8
chakirassmae <chakirassmae@gmail.com>
2024-12-18 14:59:05 +0100

    intiation

---
+++ RDX_UI_test/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",  // All HTML files in the public folder
+    "./src/**/*.{js,jsx}", // All JS files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
